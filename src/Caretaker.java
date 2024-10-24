f473e1bdca898684913c8b519d6ba03b622946e2
Abhii67534 <abhisharma4950@gmail.com>
2024-10-24 17:53:51 -0400

    Initial Commit

---
+++ src/Caretaker.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Caretaker class
+class Caretaker {
+    private List<Memento> mementoList = new ArrayList<>();
+
+    public void add(Memento state) {
+        mementoList.add(state);
+    }
+
+    public Memento get(int index) {
+        return mementoList.get(index);
+    }
+}
